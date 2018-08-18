ddf2d9355e91ed453a5fced8a03d957b6eff9175
Yuri Bochkarev <baltazar.bz@gmail.com>
2018-08-18 16:48:17 +0300

    add notes, readme, better paths

---
+++ README.md
@@ -0,0 +1,23 @@
+# CompleBox
+
+CompleBox is another completion window similar to albert, rofi and others just built using QT.
+
+## Author
+
+(c) 2018 Yuri Bochkarev
+
+### Notes
+
+Desired modes:
+
+* insert
+    * rt ticket number
+    * rt ticket: title
+    * ticket url
+    * ? insert all: ticket: title (url)
+* open rt ticket in a browser
+
+* open sheet ticket in a browser
+
+* activate browser tab
+* close browser tab
--- completebox.py
+++ completebox.py
@@ -1,8 +1,9 @@
-#!/bin/env python
+#!/usr/bin/env python
 
 import logging
 import sys
 import re
+from os.path import dirname
 
 from subprocess import check_output
 
@@ -19,9 +20,12 @@ logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 WINDOW_WIDTH = 1000
 WINDOW_HEIGHT = 30
 MAX_TICKET_LEN = 6
-CANDIDATES_FILENAME = '/mnt/big_ext4/btsync/prg/completebox/rt.candidates.tsv'
-ICON_FILENAME = '/mnt/big_ext4/btsync/prg/completebox/completebox.png'
+# CANDIDATES_FILENAME = '/mnt/big_ext4/btsync/prg/completebox/rt.candidates.tsv'
+# ICON_FILENAME = '/mnt/big_ext4/btsync/prg/completebox/completebox.png'
+CANDIDATES_FILENAME = dirname(__file__) + '/rt.candidates.tsv'
+ICON_FILENAME = dirname(__file__) + '/completebox.png'
 RX_SPACES = re.compile(r'\s+')
+ADD_TO_FONT_SIZE = 6
 
 
 def slurp_lines(filename):
@@ -39,6 +43,7 @@ class ExactMultipartFilterModel(QSortFilterProxyModel):
     This model is used to filter view by a pattern that contains words,
     separated with spaces. Each word of the pattern should be present in a row.
     """
+
     def __init__(self, parent):
         super(ExactMultipartFilterModel, self).__init__(parent)
         self._filteringRegExp = None
@@ -63,7 +68,7 @@ class ExactMultipartFilterModel(QSortFilterProxyModel):
         # return data.contains(self._filteringRegExp)
         found = self._filteringRegExp.search(data) != None
         # if found:
-            # logging.info('found: %s', data)
+        # logging.info('found: %s', data)
         return found
 
 
@@ -89,7 +94,7 @@ class MainWindow(QWidget):
             self.custom_filter.setFilterString)
 
         font = QFont()
-        font.setPointSize(font.pointSize() + 10)
+        font.setPointSize(font.pointSize() + ADD_TO_FONT_SIZE)
         self.comboxBox.setFont(font)
 
         self.completer = QCompleter(self.comboxBox.model(), self.comboxBox)
@@ -139,7 +144,14 @@ class XdoTool:
         return output.decode('utf8').strip()
 
     def send_text(self, window, text):
-        check_output(['xdotool', 'windowfocus', '--sync', window, 'type', text])
+        check_output(['xdotool', 'windowfocus',
+                      '--sync', window, 'type', text])
+
+# TODO:
+# * open ticket in xdg-open when pressed Ctrl-Enter
+# * highlight matches
+# * try doing the same using console/fzf
+# * put window above center vertically, not in the middle
 
 
 if __name__ == '__main__':
