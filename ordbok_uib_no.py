876a41d9b63fd10fc8643b99c9a8737cd599a722
Yuri Bochkarev <baltazar.bz@gmail.com>
2021-09-18 22:45:30 +0200

    ordbok_uib_no: basic fetching & caching

--- ordbok_uib_no.py
+++ ordbok_uib_no.py
@@ -3,15 +3,20 @@
 import logging
 import sys
 import re
-from os.path import dirname
+import bz2
+from os import makedirs
+from os.path import dirname, exists
+from urllib.parse import urlparse
 
 from subprocess import check_output
+from requests import get
+from bs4 import BeautifulSoup
 
 #import PyQt5
 from PyQt5.QtWidgets import (QApplication, QComboBox, QGridLayout, QVBoxLayout,
-                             QWidget, QDesktopWidget, QCompleter)
+                             QWidget, QDesktopWidget, QCompleter, QTextBrowser)
 from PyQt5.QtGui import QIcon, QFont, QStandardItemModel
-from PyQt5.QtCore import Qt, QSortFilterProxyModel, QRegExp
+from PyQt5.QtCore import Qt, QSortFilterProxyModel, QRegExp, QTimer
 
 FORMAT = '%(asctime)-15s %(levelname)s %(message)s'
 logging.basicConfig(format=FORMAT, level=logging.DEBUG)
@@ -19,13 +24,66 @@ logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 
 WINDOW_WIDTH = 1000
 WINDOW_HEIGHT = 30
+MIN_HEIGHT = 500
 MAX_TICKET_LEN = 6
 # CANDIDATES_FILENAME = '/mnt/big_ext4/btsync/prg/completebox/rt.candidates.tsv'
 # ICON_FILENAME = '/mnt/big_ext4/btsync/prg/completebox/completebox.png'
 CANDIDATES_FILENAME = dirname(__file__) + '/rt.candidates.tsv'
-ICON_FILENAME = dirname(__file__) + '/completebox.png'
+ICON_FILENAME = dirname(__file__) + '/ordbok_uib_no.png'
 RX_SPACES = re.compile(r'\s+')
 ADD_TO_FONT_SIZE = 6
+
+HTML = '''
+<div id="41772"><table class="paradigmetabell" cellspacing="0" style="margin: 25px;"><tbody><tr><th class="nobgnola"><span class="grunnord">liv</span></th><th class="nola" colspan="2">Entall</th><th class="nola" colspan="2">Flertall</th></tr><tr><th class="nobg">&nbsp;&nbsp;</th><th>Ubestemt form</th><th>Bestemt form</th><th>Ubestemt form</th><th>Bestemt form</th></tr><tr id="41772_1"><td class="ledetekst">n1</td><td class="vanlig">et liv</td><td class="vanlig">livet</td><td class="vanlig">liv</td><td class="vanlig">liva</td></tr><tr id="41772_2"><td class="ledetekst">n1</td><td class="vanlig">et liv</td><td class="vanlig">livet</td><td class="vanlig">liv</td><td class="vanlig">livene</td></tr></tbody></table></div>
+'''
+
+class HttpClient:
+    def get(self, url):
+        logging.info('http get "%s"', url)
+        result = get(url)
+        result.raise_for_status()
+        return result.text
+
+
+class CachedHttpClient:
+    def __init__(self, client, dirname):
+        self.client = client
+        self.dirname = dirname
+
+    def get(self, url):
+        path = self.get_path(self.get_key(url))
+        content = slurp(bz2.open, path)
+        if content is None:
+            logging.info('cache miss: "%s"', url)
+            content = self.client.get(url)
+            spit(bz2.open, path, content)
+        return content
+
+    def get_path(self, key):
+        return '{0}/{1}/{2}'.format(dirname(__file__), self.dirname, key)
+
+    def cleanup(self, url):
+        return re.sub(r'[^a-zA-Z0-9-_]', '', url)
+
+    def get_key(self, url):
+        parsed = urlparse(url)
+        return self.cleanup(parsed.query)
+
+
+def slurp(do_open, filename):
+    try:
+        with do_open(filename, 'rb') as file_:
+            return file_.read().decode()
+    except:
+        return None
+
+
+def spit(do_open, filename, content):
+    dir = dirname(filename)
+    if not exists(dir):
+        makedirs(dir)
+    with do_open(filename, 'wb') as file_:
+        file_.write(content.encode())
 
 
 def slurp_lines(filename):
@@ -37,6 +95,61 @@ def slurp_lines(filename):
             lines.append(line)
     return lines
 
+
+class Suggestions:
+    pass
+    # https://ordbok.uib.no/perl/lage_ordliste_liten_nr2000.cgi?spr=bokmaal&query=gam
+#
+# {query:'gam',
+# suggestions:["gaman","gamasje","gambe","gambier","gambisk","gambit","gamble","gambler","game","game","gamet","gametofytt","gamla","gamle-","gamleby","gamlefar","gamleheim","gamlehjem","gamlekjжreste","gamlemor","gamlen","gamlestev","gamletid","gamleеr","gamling","gamma","gammaglobulin","gammal","gammaldags","gammaldans","gammaldansk","gammaldansk","gammalengelsk","gammalgresk","gammalkjent","gammalkjжreste","gammalkommunist","gammalmannsaktig","gammalmodig","gammalnorsk","gammalnorsk","gammalost","gammalrosa","gammalstev","gammaltestamentlig","gammaltid","gammalvoren","gammastrеle","gammastrеling","gamme","gammel","gammel jomfru","gammel norsk mil","gammel som alle haugene","gammeldags","gammeldans","gammeldansk","gammeldansk","gammelengelsk","gammelgresk","gammelkjent","gammelkjжreste","gammelkommunist","gammelmannsaktig","gammelmodig","gammelnorsk","gammelnorsk","gammelost","gammelrosa","gammelstev","gammeltestamentlig","gammeltid","gammelvoren","gammen","gamp","gampe"],
+# data:["gaman","gamasje","gambe","gambier","gambisk","gambit","gamble","gambler","game","game","gamet","gametofytt","gamla","gamle-","gamleby","gamlefar","gamleheim","gamlehjem","gamlekjжreste","gamlemor","gamlen","gamlestev","gamletid","gamleеr","gamling","gamma","gammaglobulin","gammal","gammaldags","gammaldans","gammaldansk","gammaldansk","gammalengelsk","gammalgresk","gammalkjent","gammalkjжreste","gammalkommunist","gammalmannsaktig","gammalmodig","gammalnorsk","gammalnorsk","gammalost","gammalrosa","gammalstev","gammaltestamentlig","gammaltid","gammalvoren","gammastrеle","gammastrеling","gamme","gammel","gammel jomfru","gammel norsk mil","gammel som alle haugene","gammeldags","gammeldans","gammeldansk","gammeldansk","gammelengelsk","gammelgresk","gammelkjent","gammelkjжreste","gammelkommunist","gammelmannsaktig","gammelmodig","gammelnorsk","gammelnorsk","gammelost","gammelrosa","gammelstev","gammeltestamentlig","gammeltid","gammelvoren","gammen","gamp","gampe"]
+# }
+
+class Inflection:
+    # https://ordbok.uib.no/perl/bob_hente_paradigme.cgi?lid=41772
+    #  <div id="41772"><table class="paradigmetabell" cellspacing="0" style="margin: 25px;"><tr><th class="nobgnola"><span class="grunnord">liv</span></th><th class="nola" colspan="2">Entall</th><th class="nola" colspan="2">Flertall</th></tr><tr><th class="nobg">&nbsp;&nbsp;</th><th>Ubestemt form</th><th>Bestemt form</th><th>Ubestemt form</th><th>Bestemt form</th></tr><tr id="41772_1"><td class="ledetekst">n1</td><td class="vanlig">et liv</td><td class="vanlig">livet</td><td class="vanlig">liv</td><td class="vanlig">liva</td></tr><tr id="41772_2"><td class="ledetekst">n1</td><td class="vanlig">et liv</td><td class="vanlig">livet</td><td class="vanlig">liv</td><td class="vanlig">livene</td></tr></table></div>
+    def __init__(self, client, lid):
+        self.lid = lid
+        self.html = client.get(self.get_url(lid))
+
+    def get_url(self, lid):
+        return 'https://ordbok.uib.no/perl/bob_hente_paradigme.cgi?lid={0}'.format(lid)
+
+    def __repr__(self):
+        return f'Inflection(lid={self.lid})'
+
+class PartOfSpeech:
+    # <span class="oppsgramordklasse" onclick="vise_fullformer(&quot;8225&quot;,'bob')">adj.</span>
+    def __init__(self, client, soup):
+        self.name = soup.text
+        self.lid = None
+        self.inflection = None
+        m = re.search(r'\d+', soup['onclick'])
+        if m:
+            self.lid = m.group(0)
+            self.inflection = Inflection(client, self.lid)
+
+    def __repr__(self):
+        return f'PartOfSpeech(name="{self.name}", lid={self.lid}, inflection={self.inflection})'
+
+
+class Article:
+    def __init__(self, client, word):
+        soup = BeautifulSoup(client.get(self.get_url(word)), features='lxml')
+        parts = soup.find_all('span', {"class": "oppsgramordklasse"})
+        parts = [PartOfSpeech(client, x) for x in parts]
+        logging.info('parts: %s', parts)
+
+        self.parts = parts
+
+    def get_url(self, word: str) -> str:
+        return 'https://ordbok.uib.no/perl/ordbok.cgi?OPP={0}&ant_bokmaal=5&ant_nynorsk=5&bokmaal=+&ordbok=bokmaal'.format(word)
+
+    def __repr__(self):
+        return f'Article(parts={self.parts})'
+    # https://ordbok.uib.no/perl/ordbok.cgi?OPP=bra&ant_bokmaal=5&ant_nynorsk=5&bokmaal=+&ordbok=bokmaal
+    # <span class="oppslagsord b" id="22720">gi</span>
+    # <span class="oppsgramordklasse" onclick="vise_fullformer(&quot;8225&quot;,'bob')">adj.</span>
 
 class ExactMultipartFilterModel(QSortFilterProxyModel):
     """
@@ -81,28 +194,30 @@ class MainWindow(QWidget):
 
         self.comboxBox = QComboBox(self)
         self.comboxBox.setEditable(True)
-        # self.comboxBox.setCom
-        self.comboxBox.addItems(slurp_lines(CANDIDATES_FILENAME))
+        #self.comboxBox.addItems(slurp_lines(CANDIDATES_FILENAME))
         self.comboxBox.setMaximumWidth(WINDOW_WIDTH)
         self.comboxBox.setCurrentText('')
 
-        # self.completer.setCaseSensitivity(Qt.CaseInsensitive)
-
-        self.custom_filter = ExactMultipartFilterModel(self)
-        self.custom_filter.setSourceModel(self.comboxBox.model())
-        self.comboxBox.lineEdit().textEdited.connect(
-            self.custom_filter.setFilterString)
+        # self.custom_filter = ExactMultipartFilterModel(self)
+        # self.custom_filter.setSourceModel(self.comboxBox.model())
+        # self.comboxBox.lineEdit().textEdited.connect(
+        #     self.custom_filter.setFilterString)
 
         font = QFont()
         font.setPointSize(font.pointSize() + ADD_TO_FONT_SIZE)
         self.comboxBox.setFont(font)
 
-        self.completer = QCompleter(self.comboxBox.model(), self.comboxBox)
-        self.completer.setModel(self.custom_filter)
-        self.completer.setCompletionMode(QCompleter.UnfilteredPopupCompletion)
-        self.completer.popup().setFont(font)
+        # self.completer = QCompleter(self.comboxBox.model(), self.comboxBox)
+        # self.completer.setModel(self.custom_filter)
+        # self.completer.setCompletionMode(QCompleter.UnfilteredPopupCompletion)
+        # self.completer.popup().setFont(font)
+        #
+        # self.comboxBox.setCompleter(self.completer)
 
-        self.comboxBox.setCompleter(self.completer)
+        self.browser = QTextBrowser(self)
+        self.browser.setText(HTML)
+        self.browser.setMinimumHeight(MIN_HEIGHT)
+        self.browser.show()
 
         mainLayout = QVBoxLayout(self)
         # mainLayout.addStretch(1)
@@ -111,11 +226,14 @@ class MainWindow(QWidget):
         mainLayout.setContentsMargins(0, 0, 0, 0)
         # mainLayout.setMar
         mainLayout.addWidget(self.comboxBox)
+        mainLayout.addWidget(self.browser)
         self.setLayout(mainLayout)
 
         self.setWindowTitle('CompleteBox')
         self.resize(WINDOW_WIDTH, WINDOW_HEIGHT)
         self.setWindowIcon(QIcon(ICON_FILENAME))
+
+        QTimer.singleShot(1, self.center)
 
         self.center()
         self.show()
@@ -126,16 +244,30 @@ class MainWindow(QWidget):
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
-    def extractTicketNumber(self):
-        return self.comboxBox.currentText()[:MAX_TICKET_LEN]
+    # def extractTicketNumber(self):
+    #     return self.comboxBox.currentText()[:MAX_TICKET_LEN]
+
+    def set_text(self, text):
+        self.browser.setText(text)
 
     def keyPressEvent(self, e):
         if e.key() == Qt.Key_Escape:
             self.close()
         elif e.key() == Qt.Key_Return:
-            self.ticket = self.extractTicketNumber()
-            logging.info('ticket: %s', self.ticket)
-            self.close()
+            self.word = self.comboxBox.currentText()
+            logging.info('fetch "%s"', self.word)
+
+            client = CachedHttpClient(HttpClient(), 'cache')
+            article = Article(client, self.word)
+            print(article)
+
+            if article.parts:
+                first = article.parts[0]
+                self.set_text(first.inflection.html)
+
+            #self.ticket =  #self.extractTicketNumber()
+            #logging.info('word: %s', self.word)
+            #self.close()
 
 
 class XdoTool:
@@ -165,8 +297,8 @@ if __name__ == '__main__':
     window = MainWindow(app)
     result = app.exec()
 
-    logging.info('DONE: %s', window.ticket)
+    #logging.info('DONE: %s', window.word)
 
-    if window.ticket is not None:
-        logging.info('sending to window %s: %s', active_window, window.ticket)
-        xdo.send_text(active_window, window.ticket)
+    # if window.ticket is not None:
+    #     logging.info('sending to window %s: %s', active_window, window.ticket)
+    #     xdo.send_text(active_window, window.ticket)
